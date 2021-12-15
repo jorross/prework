3529cb49f8fd6ab1ab7a886ba18af8c9ef6d51ca
Jordan Ross <jorross@umich.edu>
2021-12-14 22:54:58 -0500

    pushing prework up to git

---
+++ Module-6/javascript.js
@@ -0,0 +1,19 @@
+function grow_function (){
+    document.getElementById('box').style.height = "250px";
+}
+
+function blue_function (){
+    document.getElementById('box').style.backgroundColor = "blue";
+}
+
+function fade_function (){
+    document.getElementById('box').style.opacity = "50%";
+}
+
+function reset_function (){
+    document.getElementById('box').style.opacity = "100%";
+    document.getElementById('box').style.height = "150px";
+    document.getElementById('box').style.width = "150px";
+    document.getElementById('box').style.backgroundColor = "orange";
+    document.getElementById('box').style.margin = "25px";
+}
